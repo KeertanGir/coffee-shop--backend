8b4d52799d20ccb3b3c9793f87ada8d61c612615
Keertan Gir <gkeertangirgmail.com>
2026-08-02 21:24:14 +0500

    Setup the Coffee project into my intellij. create a new Springboot project

---
+++ coffee-shop-project/src/main/java/org/coffeeshop/coffeeshopproject/CoffeeShopProjectApplication.java
@@ -0,0 +1,15 @@
+package org.coffeeshop.coffeeshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Application Start");
+        SpringApplication.run(CoffeeShopProjectApplication.class, args);
+    }
+
+}
+
---
+++ coffee-shop-project/src/test/java/org/coffeeshop/coffeeshopproject/CoffeeShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coffeeshop.coffeeshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
