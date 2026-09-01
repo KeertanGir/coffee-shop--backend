1eba22638a00c6c1c665b1db7ba786fcf2518e94
Keertan Gir <gkeertangirgmail.com>
2026-09-01 15:42:55 +0500

    Added GlobalExceptionHandler file

---
+++ coffee-shop-project/src/main/java/org/coffeeshop/coffeeshopproject/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package org.coffeeshop.coffeeshopproject.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
